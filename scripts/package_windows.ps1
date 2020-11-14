38829a45a9d415205c3e150fad2a8eabbc861d50
Karl Calden <kcalden323@gmail.com>
2020-11-14 00:05:14 -0500

    Add packaging script

--- scripts/package_windows.ps1
+++ scripts/package_windows.ps1
@@ -4,25 +4,25 @@ python -m venv v3
 
 # Install depencies
 pip install --upgrade pip --user
-pip install --upgrade setuptools
-pip install cffi
-pip install -r requirements.txt
-pip install cython
-python setup.py build_ext --inplace
+pip install --upgrade setuptools cffi cython pypiwin32 -r requirements.txt
 pip install https://github.com/pyinstaller/pyinstaller/tarball/develop
-pip install pypiwin32
+
+# python setup.py build_ext --inplace
 
 # Bundle program
 pyi-makespec -F --add-data "./images/*;images" --add-data "./*.png;." --add-data "./*.ico;." -w -i ./images/fablicator.ico fablicator.py
 pyinstaller --clean fablicator.spec -y
 
+# Clean release folder
+Remove-Item ".\release\*" -Force
+
 # Create installer
 iscc .\win_installer.iss
 
 # Package zip file
-$archive = @{
-    Path = ".\dist\fablicator.exe", ".\Documentation", ".\Configs"
+$archive = @{ 
+    Path = ".\dist\fablicator.exe", ".\Documentation", ".\Configs" 
     DestinationPath = ".\release\fablicator_interface-win10.zip"
 }
 
-Compress-Archive @archive
+Compress-Archive -Path ".\dist\fablicator.exe", ".\Documentation", ".\Configs" -DestinationPath ".\release\fablicator_interface-win10.zip"
